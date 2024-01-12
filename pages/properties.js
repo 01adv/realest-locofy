980655dd24a5252dd7cebc1787bd3fa08523cf57
Advaita <148793147+01adv@users.noreply.github.com>
2024-01-12 19:03:38 +0000

    db integration on going

--- pages/properties.js
+++ pages/properties.js
@@ -1,22 +1,33 @@
-import "antd/dist/antd.min.css";
+import React, { useEffect } from "react";
 import { Dropdown, Menu, Pagination } from "antd";
 import { DownOutlined } from "@ant-design/icons";
 import Header from "../components/header";
 import PropertiesGridContainer from "../components/properties-grid-container";
 import Footer from "../components/footer";
+import { createClient } from "@supabase/supabase-js";
 
-
-const defaultOrder = ['default order']
 
 const PropertiesGridView = () => {
+ 
+
+  //adura rh gya h abhi database integration
+  const client = createClient(process.env.NEXT_PUBLIC_URL, process.env.NEXT_PUBLIC_KEY)
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      const result = await client.from
+        ('properties').select('*')
+
+      
+    }
+  }, [])
+
   return (
     <div className="bg-gray-white w-full flex flex-col items-start justify-start text-center text-33xl text-gray-white font-body-regular-400">
       <Header />
       <div className="self-stretch h-60 flex flex-col items-center justify-center bg-[url('/category@3x.png')] bg-cover bg-no-repeat bg-top">
         <div className="flex flex-col items-center justify-start gap-[12px]">
-          <div className="leading-[72px] font-semibold">
-            Properties
-          </div>
+          <div className="leading-[72px] font-semibold">Properties</div>
           <div className="text-base leading-[24px] text-whitesmoke-200 font-body-regular-600">
             <span>{`Home / `}</span>
             <span className="font-medium text-gray-white">Properties</span>
@@ -24,7 +35,7 @@ const PropertiesGridView = () => {
         </div>
       </div>
       <div className="flex flex-col items-center justify-start pt-16 pb-2 gap-[95px] text-left text-base text-gray-black font-body-regular-600 lg:pl-[120px] lg:pr-[120px]">
-        <div className="w-[272px] flex flex-row  items-center justify-start">
+        <div className="w-[272px] flex flex-row items-center justify-start">
           <div className="shrink-0 flex flex-row items-end justify-start gap-[16px]">
             <div className="shrink-0 flex flex-row items-start justify-start gap-[8px]">
               <img
@@ -38,16 +49,11 @@ const PropertiesGridView = () => {
                 src="/squaresfour@2x.png"
               />
             </div>
-            {/* <div className="leading-[24px]">Sort by:</div> */}
-            {/* <Dropdown
-              // menu={defaultOrder.length ? Menu.Items(defaultOrder) : null}
-              placement="bottomLeft"
-              trigger={["hover"]}
-            >
+            {/* <Dropdown overlay={menu} placement="bottomLeft" trigger={["hover"]}>
               <a onClick={(e) => e.preventDefault()}>
-                {`Default Order `}
-                <DownOutlined />
-              </a>
+                {`Sort by: `}
+                <DownOutlined /> */}
+            {/* </a>
             </Dropdown> */}
           </div>
         </div>
